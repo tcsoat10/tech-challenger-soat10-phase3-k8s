c4be9d63ebe2e072da84f27566e77c27bb43f4f1
Kiyoshinoue <kiyoshinoue@gmail.com>
2025-05-14 02:23:06 +0000

    rollback validation of security group

--- main.tf
+++ main.tf
@@ -8,18 +8,8 @@ terraform {
   }
 }
 
-# Buscar Security Group existente
-data "aws_security_group" "existing_eks_sg" {
-  filter {
-    name   = "group-name"
-    values = ["${var.cluster_name}-sg"]
-  }
-}
-
-# Criar novo Security Group se não existir
+# Grupo de Segurança
 resource "aws_security_group" "eks_sg" {
-  count = length(data.aws_security_group.existing_eks_sg.id) == 0 ? 1 : 0
-  
   name        = "${var.cluster_name}-sg"
   vpc_id      = data.aws_vpc.vpc.id
 
@@ -30,29 +20,12 @@ resource "aws_security_group" "eks_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   # Regras de saída
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Definir qual Security Group será usado
-locals {
-  security_group_id = length(data.aws_security_group.existing_eks_sg.id) == 0 ? aws_security_group.eks_sg[0].id : data.aws_security_group.existing_eks_sg.id
-}
-
-# Usar Security Group na configuração do EKS
-resource "aws_eks_cluster" "eks" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_role.arn
-
-  vpc_config {
-    security_group_ids = [local.security_group_id]
-    subnet_ids         = data.aws_subnets.eks_subnets.ids
   }
 }
 
